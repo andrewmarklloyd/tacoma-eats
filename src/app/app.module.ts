c286ebf9aa004dbec64a2c0ff53ad3b8939c06c3
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-03-07 08:31:38 -0800

    Add spinning module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { RestaurantApiService } from './restaurant-api.service';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { RestaurantApiService } from './restaurant-api.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    Ng4LoadingSpinnerModule.forRoot()
   ],
   providers: [RestaurantApiService],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantApiService } from '../restaurant-api.service';
-
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   providers:  [ RestaurantApiService ],
@@ -13,14 +13,16 @@ export class HomeComponent implements OnInit {
   restaurants:any = [];
   headers:any = [];
 
-  constructor(private restaurantApiService: RestaurantApiService) { }
+  constructor(private restaurantApiService: RestaurantApiService,
+              private spinnerService: Ng4LoadingSpinnerService) { }
 
   ngOnInit() {
+    this.spinnerService.show();
     this.restaurants = [];
     this.restaurantApiService.getRestaurants().subscribe((result: []) => {
       this.headers = result['data'][0];
       this.restaurants = result['data'].slice(1, result.length);
-      console.log(this.restaurants)
+      this.spinnerService.hide();
     });
   }
 
