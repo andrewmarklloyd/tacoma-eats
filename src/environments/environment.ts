1b05dda5e51481ded3fdb5a421a2ebd900755b2f
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-03-09 21:30:06 -0800

    add local api file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'https://wt-f293cb80808b2b97de7ea3fe6d7984d5-0.sandbox.auth0-extend.com/tacoma-eats'
+  endpoint: 'http://localhost:4200/assets/tacoma-eats.json'
 };
 
 /*
